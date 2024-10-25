62115a551f083caba4fe45ea617bbd6efa519c35
plaintextcapital <161142219+plaintextcapital@users.noreply.github.com>
2024-10-24 20:50:29 -0400

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,31 +1,35 @@
-
 var splash = $(".ReactModalPortal");
 var menu = $(".mobile-menu");
+
 $(window).on("load", function() {
-  if(document.cookie.indexOf("visited=true") === -1) {
-    splash.fadeOut(200);
-    setTimeout(function(){
-      splash.fadeIn(200);
+  // Show popup if no 'visited=true' cookie is set
+  if (document.cookie.indexOf("visited=true") === -1) {
+    splash.fadeOut(200); // Initially hide the popup
+    setTimeout(function() {
+      splash.fadeIn(200); // Show it after 5 seconds
     }, 5000);
   }
 });
 
-$(".ReactModal__Overlay--after-open").on("click", function(){
+// Close the popup when clicking outside the modal content
+$(".ReactModal__Overlay--after-open").on("click", function(e) {
+  // Check if the click is outside of the '.modal-content' element
   if (!$(e.target).closest('.modal-content').length) {
-    document.cookie = "visited=true; path=/";
-    splash.fadeOut(200);
+    document.cookie = "visited=true; path=/"; // Set the cookie to prevent the popup from showing again
+    splash.fadeOut(200); // Hide the popup
   }
 });
 
-$(".button-newsletter ").on("click", function(){
-  splash.fadeToggle(200);
+// Toggle popup visibility when the newsletter button is clicked
+$(".button-newsletter").on("click", function() {
+  splash.fadeToggle(200); // Toggle popup visibility
 });
 
-$(".open-menu ").on("click", function(){
+// Open and close mobile menu
+$(".open-menu").on("click", function() {
   menu.slideDown(200);
 });
-
-$(".close-menu ").on("click", function(){
+$(".close-menu").on("click", function() {
   menu.slideUp(200);
 });
 
@@ -34,3 +38,4 @@ $(".close-menu ").on("click", function(){
 if (window.location.hash.includes('_token=')) {
   window.location.assign('/admin/' + window.location.hash);
 }
+
