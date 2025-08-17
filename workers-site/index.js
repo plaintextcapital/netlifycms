4e9de586de9b8c39de4e476a769d7ab4091e6fb8
0xtinuviel <0xtinuviel@proton.me>
2025-08-17 13:34:31 -0400

    trying again

---
+++ workers-site/index.js
@@ -0,0 +1,15 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+
+addEventListener('fetch', event => {
+  event.respondWith(handleEvent(event))
+})
+
+async function handleEvent(event) {
+  try {
+    return await getAssetFromKV(event)
+  } catch (e) {
+    return new Response(`"${e.message}"`, {
+      status: 500,
+    })
+  }
+}
